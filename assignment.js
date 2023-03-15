9107d4a39fa2847f3733ea35f26d7fe7bca2093a
kayitaclever <kayitaramirwaaclever@gmail.com>
2023-03-15 11:46:12 +0300

    first commit

---
+++ assignment.js
@@ -0,0 +1,31 @@
+arr1 = [3,7,34,90,12]
+let lastElement = arr1[arr1.length - 1];
+console.log(lastElement)
+
+arr2 = [true, "green", "where",12,56]
+let lastElement2=arr2[arr2.length-1]
+console.log(lastElement2)
+
+const a= ["Cow", "Bird", "Snake", "Dog"];
+console.log(a.join(" "));
+ 
+
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+  arr3.sort();
+console.log(arr3)
+
+var arr = ["apple", "mango", "apple","orange", "mango", "mango"];
+let uniquearr = [...new Set(arr)];
+console.log(uniquearr);
+
+const arr5 = ["the", "way", "x", 4];
+let find= arr5.search("way");
+if
+ (arr5.includes("way")){
+onsole.log(find);}
+else {console.log("the search word was not found");
+
+}
+let word = "sevink"
+word.sort()
+console.log(word)
